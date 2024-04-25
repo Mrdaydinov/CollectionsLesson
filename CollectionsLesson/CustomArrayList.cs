7ede78c04a1f01699335793f1763385c26ddc9f4
Murad <mrdaydinov@gmail.com>
2024-04-25 12:00:38 +0400

    init commit

---
+++ CollectionsLesson/CustomArrayList.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionsLesson
+{
+    internal class CustomArrayList
+    {
+        object[] array;
+        public int Capacity { get;private set; } = 4;
+        public int Count { get; private set; } = 0;
+
+        public CustomArrayList(int capacity)
+        {
+            Capacity = capacity;
+            array = new object[Capacity];
+        }
+
+        public void Add(object value)
+        {
+            if (Count < Capacity)
+            {
+                array[Count++] = value;
+            }
+            else
+            {
+                Capacity *= 2;
+                object[] newArray = new object[Capacity];
+                for (int i = 0; i < Count; i++)
+                {
+                    newArray[i] = array[i];
+                }
+                array = newArray;
+                array[Count++] = value;
+            }
+        }
+
+        public void Remove(object value)
+        {
+            object[] newArray = new object[Capacity];
+            bool temp = false;
+            for (int i = 0; i < Count; i++)
+            {
+                if (array[i].Equals(value))
+                {
+                    temp = true;
+                    continue;
+                }
+                else
+                {
+                    if (!temp)
+                    {
+                        newArray[i] = array[i];
+                    }
+                    else
+                        newArray[i - 1] = array[i];
+                }
+            }
+            array = newArray;
+            Count--;
+        }
+
+        public void RemoveAt(int index)
+        {
+            object[] newArray = new object[Capacity];
+            bool temp = false;
+
+            for (int i = 0; i < Count; i++)
+            {
+                if (i == index)
+                {
+                    temp = true;
+                    continue;
+                }
+                else
+                {
+                    if (!temp)
+                    {
+                        newArray[i] = array[i];
+                    }
+                    else
+                    {
+                        newArray[i - 1] = array[i];
+                    }
+                }
+            }
+            array = newArray;
+            Count--;
+        }
+
+        public void TrimToSize()
+        {
+            Capacity = Count;
+        }
+
+        public void AddRange(IList collection) // there is no indexer in ICollection interface
+        {
+            for (int i = 0; i < collection.Count; i++)
+            {
+                this.Add(collection[i]);
+            }
+        }
+
+        public object this[int index]
+        {
+            get { return array[index]; }
+            set { array[index] = value; }
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                yield return array[i];
+            }
+        }
+    }
+}
---
+++ CollectionsLesson/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+
+namespace CollectionsLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            CustomArrayList customArrayList = new CustomArrayList(4);
+
+            customArrayList.Add(1);
+            customArrayList.Add(2);
+            customArrayList.Add(3);
+
+            customArrayList.Remove(3);
+            customArrayList.RemoveAt(0);
+
+            object[] objects = { "he", "hel", "hell", "hello" };
+
+            customArrayList.AddRange(objects);
+
+            Console.WriteLine( customArrayList[3]);
+            foreach (object obj in customArrayList)
+            {
+                Console.WriteLine( obj );
+            }
+        }
+    }
+}
